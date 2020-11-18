d9823201d34ef10094fddde244b3ece6a7ac153a
jcenteno <juan.centenovil@hotmail.com>
2020-11-18 17:07:15 -0500

    Git init

---
+++ app/src/main/java/com/example/hilos_persistencia_sonido/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hilos_persistencia_sonido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
